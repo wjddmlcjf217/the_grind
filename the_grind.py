bde4d06e8aa6165550830be9856a859da1df284d
Kevin <kevin_ljeong@hotmail.com>
2021-03-25 18:15:31 -0700

    addTwoNumbers

--- the_grind.py
+++ the_grind.py
@@ -1729,6 +1729,67 @@ class Solution:
         return True
 
 
+# 2. Add Two Numbers
+# Medium
+
+# 11215
+
+# 2681
+
+# Add to List
+
+# Share
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+# Example 1:
+
+
+# Input: l1 = [2,4,3], l2 = [5,6,4]
+# Output: [7,0,8]
+# Explanation: 342 + 465 = 807.
+# Example 2:
+
+# Input: l1 = [0], l2 = [0]
+# Output: [0]
+# Example 3:
+
+# Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+# Output: [8,9,9,9,0,0,0,1]
+
+# reversing both linked lists, building an integer sum off the values, taking each decimal place and building a new linked list, then returning the reverse of it
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        first = 0
+        second = 0
+        def reverse(head):
+            prev = None
+            while head:
+                tmp = head
+                head = head.next
+                tmp.next = prev
+                prev = tmp
+            return prev
+        l1 = reverse(l1)
+        l2 = reverse(l2)
+        while l1:
+            first = first*10 + l1.val 
+            l1 = l1.next
+        while l2:
+            second = second*10 + l2.val 
+            l2 = l2.next
+        third = first + second
+        third = str(third)
+        l3 = ListNode()
+        dummy = l3
+        for i in range(len(third)):
+            l3.next = ListNode(int(third[i]))
+            l3 =l3.next
+        l3 = dummy.next
+        return reverse(l3)
 
 # Q: Given a linked list, determine if it has a cycle in it.
 
