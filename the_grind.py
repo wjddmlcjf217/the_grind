187fa4beac1b09cbf28386ea4003ce851b1aa1d6
Kevin <kevin_ljeong@hotmail.com>
2021-03-27 18:30:02 -0700

    mergeTrees

--- the_grind.py
+++ the_grind.py
@@ -2795,6 +2795,64 @@ class Solution:
                 queue.append((curr_node.right, val + curr_node.right.val))
         return False
 
+
+# 617. Merge Two Binary Trees
+# Easy
+
+# 4113
+
+# 193
+
+# Add to List
+
+# Share
+# You are given two binary trees root1 and root2.
+
+# Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
+
+# Return the merged tree.
+
+# Note: The merging process must start from the root nodes of both trees.
+
+ 
+
+# Example 1:
+
+
+# Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
+# Output: [3,4,5,5,4,null,7]
+# Example 2:
+
+# Input: root1 = [1], root2 = [1,2]
+# Output: [2,2]
+
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        if not t1:
+            return t2
+        queue = deque([(t1,t2)])
+        dummy = t1
+        while queue:
+            t1, t2 = queue.popleft()
+            if not t1 or not t2:
+                continue
+            else:
+                t1.val += t2.val
+            if not t1.left:
+                t1.left = t2.left
+            else:
+                queue.append((t1.left, t2.left))                
+            if not t1.right:
+                t1.right = t2.right
+            else:
+                queue.append((t1.right, t2.right))
+        return dummy
+
 # 98. Validate Binary Search Tree
 # Given a binary tree, determine if it is a valid binary search tree (BST).
 
