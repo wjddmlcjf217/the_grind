920a188aff956653254853b6a30d27983e88a9c0
Kevin <kevin_ljeong@hotmail.com>
2021-03-28 23:18:06 -0700

    lowestCommonAncestor

--- the_grind.py
+++ the_grind.py
@@ -2853,6 +2853,53 @@ class Solution(object):
                 queue.append((t1.right, t2.right))
         return dummy
 
+# 235. Lowest Common Ancestor of a Binary Search Tree
+# Easy
+
+# 2960
+
+# 130
+
+# Add to List
+
+# Share
+# Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+ 
+
+# Example 1:
+
+
+# Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+# Output: 6
+# Explanation: The LCA of nodes 2 and 8 is 6.
+# Example 2:
+
+
+# Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+# Output: 2
+# Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+# Example 3:
+
+# Input: root = [2,1], p = 2, q = 1
+# Output: 2
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # left node < parent, right node > parent for BST
+        curr_node = root
+        while curr_node:
+            parent = curr_node.val
+            if p.val > parent and q.val > parent:
+                curr_node = curr_node.right
+            elif p.val < parent and q.val < parent:
+                curr_node = curr_node.left
+            else:
+                return curr_node
+            
+
 # 98. Validate Binary Search Tree
 # Given a binary tree, determine if it is a valid binary search tree (BST).
 
