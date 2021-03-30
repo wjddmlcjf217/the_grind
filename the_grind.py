3a663a0c597c93301fc255f49c14e06345f0300c
Kevin <kevin_ljeong@hotmail.com>
2021-03-29 19:57:30 -0700

    kSmallestPairs bruteforce

--- the_grind.py
+++ the_grind.py
@@ -3040,3 +3040,53 @@ class MedianFinder:
             return float(self.big[0] - self.small[0]) / 2
         else:
             return float(self.big[0])
+
+# 373. Find K Pairs with Smallest Sums
+# Medium
+
+# 1860
+
+# 126
+
+# Add to List
+
+# Share
+# You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+
+# Define a pair (u, v) which consists of one element from the first array and one element from the second array.
+
+# Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+
+ 
+
+# Example 1:
+
+# Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+# Output: [[1,2],[1,4],[1,6]]
+# Explanation: The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+# Example 2:
+
+# Input: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+# Output: [[1,1],[1,1]]
+# Explanation: The first 2 pairs are returned from the sequence: [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+# Example 3:
+
+# Input: nums1 = [1,2], nums2 = [3], k = 3
+# Output: [[1,3],[2,3]]
+# Explanation: All possible pairs are returned from the sequence: [1,3],[2,3]
+
+# Brute force, O(n^2)
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        sums = []
+        output = []
+        for num_1 in nums1:
+            for num_2 in nums2:
+                sums.append([num_1, num_2])
+        sorted(sums, key=lambda x:sum(x))
+        i = 0
+        while i < k and i < len(sums):
+            output.append(sums[i])
+            i+=1
+        return output
+
