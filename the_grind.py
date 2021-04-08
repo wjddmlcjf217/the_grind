3c508a6d9b9ec095353a00021793c75408c23d61
Kevin <kevin_ljeong@hotmail.com>
2021-04-07 18:36:36 -0700

    setZeroes

--- the_grind.py
+++ the_grind.py
@@ -902,6 +902,56 @@ def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
     return converted_arr
 
 
+# 73. Set Matrix Zeroes
+# Medium
+
+# 3289
+
+# 364
+
+# Add to List
+
+# Share
+# Given an m x n matrix. If an element is 0, set its entire row and column to 0. Do it in-place.
+
+# Follow up:
+
+# A straight forward solution using O(mn) space is probably a bad idea.
+# A simple improvement uses O(m + n) space, but still not the best solution.
+# Could you devise a constant space solution?
+ 
+
+# Example 1:
+
+
+# Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+# Output: [[1,0,1],[0,0,0],[1,0,1]]
+# Example 2:
+
+
+# Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+# Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ 
+ # brute force O(m*n) way using O(m*n) space
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row = len(matrix)
+        column = len(matrix[0])
+        rows, columns = set(), set()
+        
+        for i in range(row):
+            for j in range(column):
+                if matrix[i][j] == 0:
+                    rows.add(i)
+                    columns.add(j)
+        for i in range(row):
+            for j in range(column):
+                if i in rows or j in columns:
+                    matrix[i][j] = 0
+
 
 
 # concur question
