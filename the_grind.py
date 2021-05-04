03ee6571f4d7e8231b36feb65411b8f0d04ddf84
Kevin <kevin_ljeong@hotmail.com>
2021-05-03 19:56:16 -0700

    pascals triangle

--- the_grind.py
+++ the_grind.py
@@ -1148,6 +1148,38 @@ def coin_change(n):
 
     return output
     
+# 118. Pascal's Triangle
+# Easy
+
+# Share
+# Given an integer numRows, return the first numRows of Pascal's triangle.
+
+# In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+# Example 1:
+
+# Input: numRows = 5
+# Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+# Example 2:
+
+# Input: numRows = 1
+# Output: [[1]]
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        container = []
+        for i in range(numRows):
+            # set up initial row template 
+            curr_row = [0]*(i + 1)
+            # the start and end is always 1
+            curr_row[0], curr_row[-1] = 1, 1
+            # starting from the 3rd row, or when i == 2
+            for j in range(1, len(curr_row) - 1):
+                # the inbetween values are sums of previous rows upper left and upper values
+                curr_row[j] = container[i-1][j-1] + container[i-1][j]
+            container.append(curr_row)
+        return container
 
 
 # Q: Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
